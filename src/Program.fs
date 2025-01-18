dbbbaa90e9854ee8e8c2b91136e88dee544e13e3
Vinicius Mamore <vinicius.mamore@gmail.com>
2025-01-18 16:59:42 -0400

    Create tests file

--- src/Program.fs
+++ src/Program.fs
@@ -1,14 +1,16 @@
+module Todo.App
+
 open System
-open System.Threading.Tasks
 open Giraffe
 open Giraffe.EndpointRouting
-open Giraffe.ViewEngine
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 
 type Todo = { Id: Guid; Description: string; Done: bool; CreatedAt: DateTime; DoneAt: DateTime option }
+
+type TodoRequest = { Description: string }
 
 let strToGuidOption (str: string) : Guid option =
     match Guid.TryParse str with
@@ -37,93 +39,7 @@ type TodoRepo() =
     //     let newTodo = match todo with
     //     | Some todo ->  { todo with Done = true; DoneAt = Some DateTime.UtcNow }
     //     | None _ -> None
-    
-let renderWithBaseLayout 
-    (headSlot: XmlNode list)
-    (bodySlot: XmlNode list)
-    (footerSlot: XmlNode list)
-    : XmlNode
-    = 
-    html [] [
-        head [] [
-            title [] [str "MyTodoApp"]
-            meta [
-                _charset "UTF-8";
-                _name "viewport";
-                _content "width=device-width, initial-scale=1"
-            ];
-            yield! headSlot;
-        ]
-        body [] [
-            yield! bodySlot;
-        ]
-        footer [] [
-            yield! footerSlot;
-        ]
-    ]
-    
-let renderIndexPage
-    (todoList: Todo list)
-    : Task<XmlNode>
-    =
-    task {
-        let count = todoList.Length
-
-        let itemListMarkup = 
-            div [] [
-                h3 [] [ str $"Total Count: {count}"]
-                ul [] [
-                    yield! 
-                        todoList
-                        |> List.map (fun i -> 
-                            li [] [ str (i.Description.ToString()) ]
-                        )
-                ]
-                hr []
-                a [ _href "/todo"] [ str "Create New Todo" ]
-            ]
-            
-        return 
-            renderWithBaseLayout
-                [] 
-                [ itemListMarkup ]
-                []
-    }
-    
-let renderDetailPageMarkup 
-    (item: Todo option)
-    : XmlNode 
-    =
-    let itemMarkup = 
-        div [] [
-            match item with 
-            | None -> h3 [] [ str "Not Found!" ]
-            | Some i ->
-                h3 [] [ str $"Description: {i.Description.ToString()}" ]
-                h2 [] [ str $"Created at: {i.CreatedAt}" ]
-                h2 [] [ str $"Done at: {i.DoneAt}" ]
-        ]
-     
-    renderWithBaseLayout
-        [] 
-        [ itemMarkup ]
-        []
-        
-let renderCreateTodoPageMarkup : XmlNode 
-    =
-    let createTodoMarkup = 
-        div [] [
-            form [_action "/todo"; _method "post"] [
-                input [_placeholder "Todo Description"; _type "text"; _name "description"; _id "description"]
-                button [ _type "Submit" ] [ str "Enviar" ]
-            ]
-        ]
-     
-    renderWithBaseLayout
-        [] 
-        [ createTodoMarkup ]
-        []
-    
+       
     
 // ---------------------------------
 // Web app
@@ -131,76 +47,50 @@ let renderCreateTodoPageMarkup : XmlNode
 
 let todoRepo = TodoRepo()
 
-let getAllHttpHandler =
-    handleContext( fun (ctx: HttpContext) -> 
-        task {
-            let! indexPageMarkup = renderIndexPage(todoRepo.GetAll())
+module TodoHandlers =
 
-            return! 
-                indexPageMarkup
-                |> RenderView.AsString.htmlNode
-                |> ctx.WriteHtmlStringAsync
-        }
-    )
+    let helloWorld =
+        handleContext( fun ctx -> "hello world!" |> ctx.WriteTextAsync)
     
-let detailHttpHandler
-    (id: string)
-    =
-    handleContext( fun (ctx: HttpContext) -> 
-        task {
+    let getAll =
+        handleContext( fun ctx -> todoRepo.GetAll() |> ctx.WriteJsonAsync)
 
-            let item = todoRepo.GetOne(id)
-            let detailPageMarkup = renderDetailPageMarkup item
+    let getOne (id: string) =
+        handleContext( fun ctx ->
+            task {
+                Console.WriteLine $"Id = {id}"
+                let todo = todoRepo.GetOne id
+                return! ctx.WriteJsonAsync todo
+            }
+        )
 
-            return! 
-                detailPageMarkup
-                |> RenderView.AsString.htmlNode
-                |> ctx.WriteHtmlStringAsync
-        }
-    )
-    
-let showCreateHttpHandler=
-    handleContext( fun (ctx: HttpContext) -> 
-        task {
-            return! 
-                renderCreateTodoPageMarkup
-                |> RenderView.AsString.htmlNode
-                |> ctx.WriteHtmlStringAsync
-        }
-    )
-    
-let createTodoHttpHandler=
-    handleContext( fun (ctx: HttpContext) -> 
-        task {
-            let description = ctx.Request.Form.Item "description" 
-            
-            todoRepo.Create description |> ignore
-                
-            let! indexPageMarkup = renderIndexPage(todoRepo.GetAll())
-            
-            return! 
-                indexPageMarkup
-                |> RenderView.AsString.htmlNode
-                |> ctx.WriteHtmlStringAsync
-        }
-    )
+    let create=
+        handleContext( fun (ctx: HttpContext) -> 
+            task {
+                let! todo = ctx.BindJsonAsync<TodoRequest>()
+                todoRepo.Create todo.Description |> ignore
+                return! todoRepo.GetAll() |> ctx.WriteJsonAsync
+            }
+        )
     
 let endpoints =
     [ 
         GET [ 
-            route "/" (getAllHttpHandler)
-            route "/todo" (showCreateHttpHandler)
-            routef "/detail/%s" detailHttpHandler   
+            route "/hello" TodoHandlers.helloWorld
+            route "/todo" TodoHandlers.getAll
+            routef "/todo/%s" TodoHandlers.getOne   
         ]
         POST [
-            route "/todo" (createTodoHttpHandler)
+            route "/todo" TodoHandlers.create
         ]
     ]
 
 let notFoundHandler = "Not Found" |> text |> RequestErrors.notFound
     
 let configureApp (app: IApplicationBuilder) =
-    app.UseRouting().UseGiraffe(endpoints).UseGiraffe(notFoundHandler)
+    app.UseRouting()
+       .UseGiraffe(endpoints)
+       .UseGiraffe(notFoundHandler)
     
 let configureServices (services : IServiceCollection) =
     services.AddRouting().AddGiraffe() |> ignore
---
+++ tests/Tests.fs
@@ -0,0 +1,32 @@
+﻿module Todo.Tests
+
+open System
+open Xunit
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.TestHost
+open Microsoft.AspNetCore.Hosting
+open System.Net.Http
+open System.Net
+
+let createHost() =
+    WebHostBuilder()
+        .UseTestServer()
+        .Configure(Action<IApplicationBuilder> App.configureApp)
+        .ConfigureServices(App.configureServices)
+        .UseUrls("localhost:5005")
+
+let testRequest (request : HttpRequestMessage) =
+    let resp = task {
+        use server = new TestServer(createHost())
+        use client = server.CreateClient()
+        let! response = request |> client.SendAsync
+        return response
+    }
+    resp.Result
+
+[<Fact>]
+let ``Hello world endpoint says hello`` () =
+    let response = testRequest (new HttpRequestMessage(HttpMethod.Get, "/hello"))
+    let content = response.Content.ReadAsStringAsync().Result
+    Assert.Equal(response.StatusCode, HttpStatusCode.OK)
+    Assert.Equal(content, "hello world!")
